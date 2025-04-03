bdc56bdb00452d727a41b87610a12acd5c42ba52
bryanavalossistemas <bryanavalossistemas@gmail.com>
2025-04-03 13:18:36 -0500

    qutando comentarios

--- src/schemas/products.ts
+++ src/schemas/products.ts
@@ -13,9 +13,6 @@ export const ProductSchema = z.object({
   ecommercePercentageDiscount: z.number().int(),
   ecommerceSalePrice: z.number(),
   images: z.array(z.object({ id: z.number().int(), path: z.string() })),
-  // stock: z.number().int(),
-  // brandId: z.number().int().nullable(),
-  // images: z.array(z.object({ id: z.number().int(), path: z.string() })).optional(),
 });
 
 export type Product = z.infer<typeof ProductSchema>;
